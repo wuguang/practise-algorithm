ad6b9e7f9747f4c4ce9919e81265bc5137ec0ff3
wuguang <wuguang@wxblockchain.com>
2021-09-29 18:33:19 +0800

    update quicksort

--- practise/sort.js
+++ practise/sort.js
@@ -67,9 +67,9 @@ class JsSort{
                     if(list[j]<list[j-1]){
                         console.log(`list[j] = ${list[j]}----list[j-1] = ${list[j-1]}`);
                         this.swap(list,j,j-1);
-                        console.log(JSON.stringify(list));
+                        
                         swapCount ++;
-                        break;
+                        //break;
                     }
                 }
             }
@@ -83,13 +83,100 @@ class JsSort{
         return list;
     }
 
+    shellSort(list){
+        let swapCount = 0;
+        let compareCount = 0;
+        /*
+        if(Array.isArray(list) && list.length>1){
+            let len = list.length;
+            //变多
+            let gap = Math.ceil(len/2);
+            while(gap>0){
+                let group = Math.floor(len/gap);
+                //i 提供组
+                for(let i=0;i<group;i++){
+                    for(let j=i*gap+i;j>0;j-=gap){
+                        
+                        if(list[j]<list[j-1]){
+                            this.swap(list,j,j-1);
+                        }
+                        
+                    }
+                }
+                //变少/3是什么选择??
+                gap = Math.floor(gap/3);
+            }
+        }
+        */
+        return list;
+    }
 
+    initArrForQuickSort(arr){
+        let len = arr.length;
+        if(len>2){
+            let middle = Math.floor(len/2);
+            if(arr[0]>arr[middle]){
+                this.swap(arr,0,middle);
+            }
+            if(arr[middle]>arr[len-1]){
+                this.swap(arr,middle,len-1);
+            }
+            this.swap(arr,middle,len-1);
+        }
+        return arr;
+    }
 
+    quickSort(list){
+        let swapCount = 0;
+        let compareCount = 0;
 
+        let doQucik = list=>{
+            list = this.initArrForQuickSort(list);
+            let len = list.length;
+            let left = 1;
+            let right = len-3;
+            let target = list[len-2];
+            while(left !==right){
+                while(list[left]<=target){
+                    left++;
+                }
+                while(list[right]>=target){
+                    right++;
+                    //console.log(`right = ${right}`);
+                }
+                if(left !== right){
+
+                    console.log(` this.swap----left = ${left}---right = ${right}`);
+                    console.log(`before list[left] = ${list[left]}-----list[right] = ${list[right]}`);
+                    this.swap(list,left,right);
+                    console.log(`after list[left] = ${list[left]}-----list[right] = ${list[right]}`);
+                }
+                break;
+
+            }
+
+            if(list[left]>=target){
+                this.swap(list,left,len-2);
+                list = [...doQucik(list.slice(0,left)),target,...doQucik(list.slice(left+1,len))];
+
+            }else{
+                this.swap(list,left+1,len-2);
+                list = [...doQucik(list.slice(0,left+1)),target,...doQucik(list.slice(left+2,len))];
+            }
+            //一轮排序完成
+            return list;
+        }
+
+        if(Array.isArray(list) && list.length>1){
+            list = doQucik(list);
+        }
+        console.log(JSON.stringify(list));
+        return list;
+    }
 }
 
 
 //test
 let mySort = new JsSort();
 
-mySort.insertSort([2,3,52,4,576,8,982,23,23,34]);
+mySort.quickSort([2,12,2,3,12,2,4,12,14,16,17,3,52,4,576,8,982,28,23,34]);
